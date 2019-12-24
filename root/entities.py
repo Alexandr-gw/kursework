ee2396003ad557ad3f0554f21faad547dcf0e18b
vovapasko <vovapasko@gmail.com>
2019-12-24 16:50:32 +0200

    Created entities

--- root/entities.py
+++ root/entities.py
@@ -17,3 +17,25 @@ class Doctor(Base):
 
 class Patient(Base):
     __tablename__ = "doctor"
+    username = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    birthdate = Column(TIMESTAMP, nullable=False)
+    sex = Column(String, nullable=False)
+
+
+class Symptom(Base):
+    __tablename__ = 'symptom'
+    symptom_name = Column(String, primary_key=True)
+
+
+class Contraindications(Base):
+    name = Column(String, primary_key=True)
+    additional_info = Column(String, nullable=True)
+
+
+class Drug(Base):
+    drug_name = Column(String, primary_key=True)
+    price = Column(Float, nullable=False)
+    symptom_name = Column(String, ForeignKey(Symptom.symptom_name, ondelete="cascade"), nullable=False)
+    contraindication = Column(String, ForeignKey(Contraindications.name, ondelete="cascade"), nullable=False)
