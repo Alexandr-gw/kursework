2318c34d593565d24e1ace308027b0f320e8f324
Alexandr-gw <profi_d107@rambler.ru>
2019-12-24 17:57:51 +0200

    Creaeted doctor and patient pages

--- root/app.py
+++ root/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request, url_for, render_template, Flask, session
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 SECRET_KEY = "Secret key"
@@ -8,14 +9,28 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def hello():
-    return render_template('start_page.html')
+    return render_template('login.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        return "Logged in"
-    return "Here"
+        if request.form['username'] == 'doctor' and request.form['password'] == 'doctor':
+            return redirect('/doctor')
+        elif request.form['username'] == 'patient' and request.form['password'] == 'patient':
+            return redirect('/patient')
+        error = 'Invalid Credentials. Please try again.'
+    return render_template('login.html', error=error)
+
+@app.route('/patient')
+def patient():
+    return render_template('patient_page.html')
+
+
+@app.route('/doctor')
+def doctor():
+    return render_template('doctor_page.html')
 
 
 if __name__ == '__main__':
