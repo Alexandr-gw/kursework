18ebdb3574671602df23552e9f8472d20f6dd017
Alexandr-gw <profi_d107@rambler.ru>
2019-12-24 17:40:28 +0200

    app.py updated

--- root/app.py
+++ root/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request, url_for, render_template, Flask, session
 
 app = Flask(__name__)
 SECRET_KEY = "Secret key"
@@ -7,4 +8,13 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def hello():
-    return
+    return render_template('start_page.html')
+
+
+@app.route('/login')
+def login():
+    return "Logged in"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
